d0a165d597ca1bc68f15f5a1ebc4b946edc95063
Yurii Voitiuk <voutuk@icloud.com>
2025-02-12 18:38:40 +0200

    Add Rover

--- OLX_Dyplom_ADM/Tf3.0/run.sh
+++ OLX_Dyplom_ADM/Tf3.0/run.sh
@@ -7,7 +7,8 @@ terraform apply -target=module.build_agent_instance -auto-approve
 terraform apply -target=module.aks_cluster -auto-approve
 terraform apply -target=module.backup_storage -auto-approve
 terraform apply -target=module.ansible_inventory -auto-approve
-terraform apply -target=module.rule_block_ssh -auto-approve       #INFO: Block SSH use after ansible playbook
+terraform apply -target=module.main_rule_block_ssh -auto-approve       #INFO: Block SSH use after ansible playbook
+terraform apply -target=module.agent_rule_block_ssh -auto-approve      #INFO: Block SSH use after ansible playbook
 
 # terraform refresh
 terraform state list
