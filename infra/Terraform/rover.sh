a3198931c2167a6b048964ce23343c703de3436d
Yurii Voitiuk <voutuk@icloud.com>
2025-03-29 12:17:51 +0200

    * fix2

--- infra/Terraform/rover.sh
+++ infra/Terraform/rover.sh
@@ -1,4 +1,4 @@
-terragrunt plan -out plan.out
-terragrunt show -json plan.out > plan.json
+doppler run -- terragrunt plan -out plan.out
+doppler run -- terragrunt show -json plan.out > plan.json
 MSYS_NO_PATHCONV=1 docker run --rm -it -p 9000:9000 -v "$(pwd)":/src/ im2nguyen/rover:latest -planJSONPath=plan.json -standalone true
 rm plan.out plan.json
